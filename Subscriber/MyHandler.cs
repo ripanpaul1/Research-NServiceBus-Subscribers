cfb4d4600abf40907a4d99175cd9cd928635ccb0
Ripan Paul <ripan.paul@lateetud.com>
2017-11-22 20:10:56 +0530

    test build

---
+++ Subscriber/MyHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Messages;
+using NServiceBus.Logging;
+
+public class MyHandler :
+    IHandleMessages<RowMessage>
+{
+
+    static ILog log = LogManager.GetLogger<MyHandler>();
+
+    public Task Handle(RowMessage message, IMessageHandlerContext context)
+    {
+        log.Info($"Subscriber has received RowMessage event with OrderId {message.Message}.");
+        return Task.CompletedTask;
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,57 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus.Persistence.Sql;
+using System.Data.SqlClient;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitiazeQueue("testqueue.Subscriber", "testqueue.Publisher").GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        // InitiazeQueue
+        static async Task InitiazeQueue(string EndpointName_Subscriber, string EndpointName_Publisher)
+        {
+            var endpointConfiguration = new EndpointConfiguration(EndpointName_Subscriber);
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+            var connection = @"Data Source=RIPANPC\SqlExpress;Initial Catalog=TestMsmqDB;UID=sa;Password=Password123;Integrated Security=True";
+            persistence.SqlVariant(SqlVariant.MsSqlServer);
+            persistence.ConnectionBuilder(
+                connectionBuilder: () =>
+                {
+                    return new SqlConnection(connection);
+                });
+
+            var subscriptions = persistence.SubscriptionSettings();
+            subscriptions.CacheFor(TimeSpan.FromDays(1));
+
+            var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+            var routing = transport.Routing();
+            routing.RegisterPublisher(
+                assembly: typeof(RowMessage).Assembly,
+                publisherEndpoint: EndpointName_Publisher);
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
