c35ef500d8652981e9aaa82175cab4865160fe10
Ripan Paul <ripan.paul@lateetud.com>
2017-12-01 20:01:54 +0530

    changes done for NECGeneralAgent

--- Lateetud.NServiceBus.Subscriber/Program.cs
+++ Lateetud.NServiceBus.Subscriber/Program.cs
@@ -1,11 +1,9 @@
-﻿using NServiceBus;
+﻿
 using System;
-using System.Threading.Tasks;
-using Lateetud.NServiceBus.Common;
-using NServiceBus.Persistence.Sql;
-using System.Data.SqlClient;
 using System.Configuration;
 using System.Collections.Generic;
+using Lateetud.NServiceBus.Common;
+using Lateetud.NServiceBus.Common.Models.NECGeneralAgent;
 
 namespace Lateetud.NServiceBus.Subscriber
 {
@@ -16,28 +14,15 @@ namespace Lateetud.NServiceBus.Subscriber
             MsmqSqlDBConfiguration msmqsqldbconfig = new MsmqSqlDBConfiguration(ConfigurationManager.ConnectionStrings["SqlPersistence"].ConnectionString);
             
 
-            // set to register Publisher endpoints to Subscribers end
             List<PublisherEndpoints> publisherEndpoints = new List<PublisherEndpoints>();
-            publisherEndpoints.Add(new PublisherEndpoints(endpointName: "queue1.publisher", messageType: typeof(TestMessage)));
-
-            // if queue does not exists, created & got pipeline
-            var endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("queue2.subscriber", publisherEndpoints);
+            publisherEndpoints.Add(new PublisherEndpoints(endpointName: "NEC.GeneralAgent.Publisher", messageType: typeof(NECGeneralAgent)));
+            var endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("NEC.GeneralAgent.Subscriber", publisherEndpoints);
             msmqsqldbconfig.StartEndpoint(endpointConfiguration).GetAwaiter().GetResult();
 
-            // if queue does not exists, created & got pipeline
-            endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("queue3.subscriber", publisherEndpoints);
+            publisherEndpoints.Clear();
+            publisherEndpoints.Add(new PublisherEndpoints(endpointName: "NEC.GeneralAgent.Publisher", messageType: typeof(NECGeneralAgentResult)));
+            endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("NEC.GeneralAgent.Subscriber", publisherEndpoints);
             msmqsqldbconfig.StartEndpoint(endpointConfiguration).GetAwaiter().GetResult();
-
-
-            // set to register Publisher endpoints to Subscribers end
-            publisherEndpoints = new List<PublisherEndpoints>();
-            publisherEndpoints.Add(new PublisherEndpoints(endpointName: "queue2.publisher", messageType: typeof(TestMessage)));
-            publisherEndpoints.Add(new PublisherEndpoints(endpointName: "queue3.publisher", messageType: typeof(TestMessage)));
-
-            // if queue does not exists, created & got pipeline
-            endpointConfiguration = msmqsqldbconfig.ConfigureEndpoint("queue1.subscriber", publisherEndpoints);
-            msmqsqldbconfig.StartEndpoint(endpointConfiguration).GetAwaiter().GetResult();
-
 
 
             Console.WriteLine("Press any key to exit");
---
+++ Lateetud.NServiceBus.Subscriber/handlers/NECGeneralAgent/NECGeneralAgentHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Lateetud.NServiceBus.Common.Models.NECGeneralAgent;
+using NServiceBus.Logging;
+
+public class NECGeneralAgentHandler :
+    IHandleMessages<NECGeneralAgent>
+{
+    static ILog log = LogManager.GetLogger<NECGeneralAgentHandler>();
+    public Task Handle(NECGeneralAgent message, IMessageHandlerContext context)
+    {
+        log.Info($"NECGeneralAgent: {message.Message}.");
+        return Task.CompletedTask;
+    }
+}
+
+public class NECGeneralAgentResultHandler :
+    IHandleMessages<NECGeneralAgentResult>
+{
+    static ILog log = LogManager.GetLogger<NECGeneralAgentResultHandler>();
+    public Task Handle(NECGeneralAgentResult message, IMessageHandlerContext context)
+    {
+        log.Info($"NECGeneralAgentResult: {message.Message}.");
+        return Task.CompletedTask;
+    }
+}
